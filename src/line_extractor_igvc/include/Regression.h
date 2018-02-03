930eaf8978c69883d5bb4949939fcec714275e54
Min Gyo Kim <mingyo.kim3@gmail.com>
2018-02-03 11:08:06 -0800

    Create regression class

---
+++ src/line_extractor_igvc/include/Regression.h
@@ -0,0 +1,17 @@
+/*
+ * Created By: Min Gyo Kim
+ * Created On: January 20, 2018
+ * Description: Class declaration for Regression, which calculates
+ *              line of best fit for each cluster of points
+ */
+
+#ifndef PROJECT_REGRESSION_H
+#define PROJECT_REGRESSION_H
+
+
+class Regression {
+
+};
+
+
+#endif //PROJECT_REGRESSION_H
---
+++ src/line_extractor_igvc/src/Regression.cpp
@@ -0,0 +1,7 @@
+/*
+ * Created By: Min Gyo Kim
+ * Created On: February 3, 2018
+ * Description: Calculates line of best fit for each cluster of points
+ */
+
+#include <Regression.h>
